9eb3b348a2a358ecd1f8b3f204b8fa5893125721
DragonTamer13 <wynekids@yahoo.com>
2025-02-18 22:53:44 -0500

    level generator script

---
+++ Scripts/2D/random_item_generation.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var object: Array[NodePath] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var path: NodePath = object.pick_random()
+	var obj = get_node(path)
+	obj.visible = true
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
