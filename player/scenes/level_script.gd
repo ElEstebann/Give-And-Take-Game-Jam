2da5ff75ca3f2b6c32ed04b54c35340d7293db79
DanyeWestinator <44176712+DanyeWestinator@users.noreply.github.com>
2025-02-20 17:55:16 -0800

    input push

---
+++ player/scenes/level_script.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var nav_region : NavigationRegion2D = $NavigationRegion2D
+@onready var present01 : Node2D = $Present
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var polygon = nav_region.navigation_polygon
+	var verts = polygon.get_vertices()
+	for vert in verts:
+		print("Started with vert: ", vert)
+	for corner in [Vector2(-1, -1), Vector2(-1, 1), Vector2(1, -1), Vector2(1, 1)]:
+		var vert = present01.global_position + corner * 4
+		verts.append(vert)
+		print("Added vert: ", vert)
+	
+	polygon.set_vertices(verts)
+	nav_region.bake_navigation_polygon(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
