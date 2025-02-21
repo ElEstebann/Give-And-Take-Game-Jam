ad62d52c89c68e6eb0edbb5775c02fbc37fa47a0
Esteban Aldana <estebanaaldana@gmail.com>
2025-02-20 17:56:16 -0800

    Reimport & made sky scroll

--- Scripts/3d/random_house.gd
+++ Scripts/3d/random_house.gd
@@ -2,28 +2,9 @@ extends Node3D
 
 
 
+@onready var homes = $Homes.get_children()
 
-@onready var home_list = [$Homes/house_type022,
- $Homes/house_type032,
- $Homes/house_type042,
- $Homes/house_type052,
- $Homes/house_type062,
- $Homes/house_type072,
- $Homes/house_type082,
- $Homes/house_type092,
- $Homes/house_type102,
- $Homes/house_type112,
- $Homes/house_type122,
- $Homes/house_type132,
- $Homes/house_type142,
- $Homes/house_type152,
- $Homes/house_type162,
- $Homes/house_type172,
- $Homes/house_type182,
- $Homes/house_type192,
- $Homes/house_type202,
- $Homes/house_type212,
- $Homes/house_type012]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -35,7 +16,8 @@ func _process(delta: float) -> void:
 	pass
 
 func randomize_home() -> void:
-	for item in home_list:
+	for item in homes:
 		item.visible = false
-	home_list[randi_range(0,home_list.size()-1)].visible = true
+		
+	homes[randi_range(0,homes.size()-1)].visible = true
 	print_debug("House Randomized")
