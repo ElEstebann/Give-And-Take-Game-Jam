20b589c1c163625a26559da0b8585cbbd68d5c9b
Esteban Aldana <estebanaaldana@gmail.com>
2025-02-22 23:07:12 -0800

    Readjusted 3d time

--- Scripts/3d/3d_run_game.gd
+++ Scripts/3d/3d_run_game.gd
@@ -19,7 +19,7 @@ var street_suffixes = [
 	"Lane", "Way", "Court", "Place", "Terrace"
 ]
 
-var time_left = 30
+var time_left = 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
